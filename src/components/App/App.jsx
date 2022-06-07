06a115925ac2a092ad0641b47df127cedcfd6c14
Andriy Kutushonok <kutushonok593@gmail.com>
2022-06-08 00:22:15 +0300

    hw-fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,12 +5,11 @@ import Section from '../Section/Section';
 import Notification from '../Notification/Notification';
 
 class App extends Component {
-  
     state = {
         good: 0,
         neutral: 0,
-        bad: 0
-    }
+        bad: 0,
+    };
 
     leaveFeedback = option => {
         this.setState(prevState => ({
@@ -20,42 +19,44 @@ class App extends Component {
 
     countTotalFeedback = () => {
         return Object.values(this.state).reduce((total, value) => {
-            return total += value;;
+            return (total += value);
         }, 0);
     };
-    
+
     countPositiveFeedbackPercentage = () => {
-        return Math.trunc(this.state.good / this.countTotalFeedback() * 100);
-    }
-    render(){
+        return Math.trunc((this.state.good / this.countTotalFeedback()) * 100);
+    };
+    render() {
         const totalFeedback = this.countTotalFeedback();
         const percentage = this.countPositiveFeedbackPercentage();
-        const options = Object.keys(this.state)
-        
-        return(
-            <div className=''>
+        const options = Object.keys(this.state);
+
+        return (
+            <div className="">
                 <Section title="Please leave feedback">
-                    <FeedbackOptions 
+                    <FeedbackOptions
                         options={options}
                         onLeaveFeedback={this.leaveFeedback}
                     />
                 </Section>
                 <Section title="Statistics">
                     {totalFeedback > 0 && (
-                        <Statistics 
-                            good={this.state.good} 
+                        <Statistics
+                            good={this.state.good}
                             neutral={this.state.neutral}
-                            bad={this.state.bad} 
-                            total={totalFeedback} 
+                            bad={this.state.bad}
+                            total={totalFeedback}
                             positivePercentage={percentage}
-                        /> 
+                        />
                     )}
-                    
-                    {totalFeedback < 1 && <Notification message="There is no feedback"/>}
+
+                    {totalFeedback < 1 && (
+                        <Notification message="There is no feedback" />
+                    )}
                 </Section>
             </div>
-        )
+        );
     }
-};
+}
 
-export default App
+export default App;
